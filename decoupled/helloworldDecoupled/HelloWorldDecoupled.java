5b7ebed78bbd56ef41a093888b209e1fed451b48
Yummy.Yang <cn.yummy@gmail.com>
2013-05-15 01:28:28 +0800

    seperate the helloworldDecoupled codes.

--- decoupled/helloworldDecoupled/HelloWorldDecoupled.java
+++ decoupled/helloworldDecoupled/HelloWorldDecoupled.java
@@ -1,8 +1,8 @@
 package yummy.test;
 public class HelloWorldDecoupled{
 	public static void main(String[] args){
-		MessageRenderer mr = new StandardOutMessageRenderer();
-		MessageProvider mp = new HelloWorldMessageProvider();
+		IMessageRenderer mr = new StandardOutMessageRenderer();
+		IMessageProvider mp = new HelloWorldMessageProvider();
 		mr.setMessageProvider(mp);
 		mr.render();
 	}
