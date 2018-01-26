9242e923ab6ae47872b42138809d2fa95718adc8
YummyYang <cn.yummy@gmail.com>
2018-01-26 18:11:48 +0800

    add CatOrNot.py

---
+++ NG/s1w2_NN_with_logisticRegression/CatOrNot.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import scipy
+from PIL import Image
+from scipy import ndimage
+from lr_utils import load_dataset
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+for i in range(10):
+	a = input("index:")
+	index = int(a)
+	plt.imshow(train_set_x_orig[index])
+	plt.show()
+	print ("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' picture.")
