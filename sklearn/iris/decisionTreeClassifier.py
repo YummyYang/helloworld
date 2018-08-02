e6906c2c6bf7f27596768ab3f5edf85d07796e52
YummyYang <cn.yummy@gmail.com>
2018-08-02 18:22:55 +0800

    add decisionTreeClassifier

---
+++ sklearn/iris/decisionTreeClassifier.py
@@ -0,0 +1,32 @@
+from sklearn.datasets import load_iris
+from sklearn import tree
+from sklearn.cross_validation import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
+
+n_classes = 3
+plot_colors = "bry"
+
+iris = load_iris()
+x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size = 0.25)
+print(y_test)
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(x_train, y_train)		# I think this pleace is better if we use different variable name of 'clf';
+
+y_predict = clf.predict(x_test)
+
+X=x_test[:,0]
+Y=x_test[:,3]
+
+plt.xlabel(iris.feature_names[1])
+plt.ylabel(iris.feature_names[2])
+plt.axis("tight")
+
+for i , color in zip(range(n_classes), plot_colors):
+	idx = np.where(y_predict == i)
+	plt.scatter(X[idx], Y[idx], c=color, label=iris.target_names[i], marker='+')
+
+plt.axis("tight")
+plt.suptitle("DCT")
+plt.legend()
+plt.show()
