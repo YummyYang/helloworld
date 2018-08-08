35043b9ef5596592726370466f85ab8c7057c3cd
YummyYang <cn.yummy@gmail.com>
2018-08-08 16:17:01 +0800

    add accuracy_score  by sklearn.metrics accuracy_score.

--- sklearn/iris/decisionTreeClassifier.py
+++ sklearn/iris/decisionTreeClassifier.py
@@ -1,19 +1,19 @@
 from sklearn.datasets import load_iris
 from sklearn import tree
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split #from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score
 import numpy as np
 import matplotlib.pyplot as plt
-
 n_classes = 3
 plot_colors = "bry"
-
 iris = load_iris()
 x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size = 0.25)
-print(y_test)
+
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(x_train, y_train)		# I think this pleace is better if we use different variable name of 'clf';
 
 y_predict = clf.predict(x_test)
+print("accuracy_score:" + str(accuracy_score(y_predict, y_test) * 100 ) + "%" )
 
 X=x_test[:,0]
 Y=x_test[:,3]
